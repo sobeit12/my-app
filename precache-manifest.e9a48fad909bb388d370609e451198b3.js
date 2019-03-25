9ccb0e0a7702f122f1ffe0b4a21cd13c45a9903a
luff <sobeit12@gmail.com>
2019-03-26 01:18:36 +0900

    Updates

---
+++ precache-manifest.e9a48fad909bb388d370609e451198b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "cf9a419e479d82085e14",
+    "url": "/my-app/static/js/main.a120e70a.chunk.js"
+  },
+  {
+    "revision": "f26ae2ba1199b685be6b",
+    "url": "/my-app/static/js/2.1d254b63.chunk.js"
+  },
+  {
+    "revision": "cf9a419e479d82085e14",
+    "url": "/my-app/static/css/main.11d87417.chunk.css"
+  },
+  {
+    "revision": "84b6622b454e1e6b6553ba9272580bd2",
+    "url": "/my-app/index.html"
+  }
+];
